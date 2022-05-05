362c7e093de20882512bf64de06e3f8f96354539
SurapallyDivya <101784043+SurapallyDivya@users.noreply.github.com>
2022-05-05 09:17:34 +0530

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.databinding2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.textSource="hello Data Binding!"
+    }
+}
